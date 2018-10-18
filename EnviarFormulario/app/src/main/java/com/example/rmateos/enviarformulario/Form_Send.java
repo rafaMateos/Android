938be308513929525c7d04bf0a0e3d349d665e5b
RafaMateos <rafaelmateostorres@gmail.com>
2018-10-18 11:09:15 +0200

    Formulario

---
+++ EnviarFormulario/app/src/main/java/com/example/rmateos/enviarformulario/Form_Send.java
@@ -0,0 +1,34 @@
+package com.example.rmateos.enviarformulario;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Form_Send extends AppCompatActivity {
+
+    Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form__send);
+
+        login = (Button)findViewById(R.id.buttonLogin);
+        Activity pant = new Activity();
+        pant.
+
+    }
+    public void Login(View v){
+
+
+        Intent ListSong = new Intent(v.getContext(),);
+        startActivity(ListSong);
+
+    }
+
+
+}
